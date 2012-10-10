fc8312d0f77bb16557030b65803fe81320e51c78
Devin Torres <devin@devintorr.es>
2012-10-10 16:13:27 -0500

    Add EXPM package definitions

---
+++ package.exs
@@ -0,0 +1,10 @@
+Expm.Package.new(
+  name: "poolboy",
+  description: "A hunky Erlang worker pool factory",
+  homepage: "http://devinus.github.com/poolboy/",
+  version: "0.9.1",
+  keywords: ["Erlang", "library", "pool", "pools", "pooler"],
+  maintainers: [[name: "Devin Torres", email: "devin@devintorr.es"],
+                [name: "Andrew Thompson", email: "andrew@hijacked.us"]],
+  repositories: [[github: "devinus/poolboy", tag: "0.9.1"]]
+)
