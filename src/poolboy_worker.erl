5bbfc1a6cd87489fb435d3f9c479cea3867c4353
David Robakowski <david.robakowski@synlay.com>
2014-01-14 10:42:13 +0100

    Replaced behaviour_info/1 with new -callback specs WARNING: this commit breaks compatibility with          Erlang releases prior to R15

--- src/poolboy_worker.erl
+++ src/poolboy_worker.erl
@@ -2,22 +2,9 @@
 
 -module(poolboy_worker).
 
--ifdef(have_callback_support).
-
 -callback start_link(WorkerArgs) -> {ok, Pid} |
                                     {error, {already_started, Pid}} |
                                     {error, Reason} when
     WorkerArgs :: proplists:proplist(),
     Pid        :: pid(),
     Reason     :: term().
-
--else.
-
--export([behaviour_info/1]).
-
-behaviour_info(callbacks) ->
-    [{start_link, 1}];
-behaviour_info(_Other) ->
-    undefined.
-
--endif.
