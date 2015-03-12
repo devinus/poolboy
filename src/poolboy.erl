3170cea215b805b0f8551fc02ed793d1ecab3ced
Bach Le <thebullno1@gmail.com>
2015-03-12 18:27:16 +0800

    Export pool/0 type
    This is to allow libraries that calls poolboy to specify their function typespecs.

--- src/poolboy.erl
+++ src/poolboy.erl
@@ -8,6 +8,7 @@
          start_link/1, start_link/2, stop/1, status/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
+-export_type([pool/0]).
 
 -define(TIMEOUT, 5000).
 
