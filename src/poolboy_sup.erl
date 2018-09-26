0cca3628e80773ad58ff8fc55724188c80927eb3
Matyas Markovics <matyas@scrapinghub.com>
2018-09-27 00:54:27 +0200

    ability to define worker supervisor in pool args, register worker supervisor as poolboy_sup

--- src/poolboy_sup.erl
+++ src/poolboy_sup.erl
@@ -6,7 +6,7 @@
 -export([start_link/2, init/1]).
 
 start_link(Mod, Args) ->
-    supervisor:start_link(?MODULE, {Mod, Args}).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, {Mod, Args}).
 
 init({Mod, Args}) ->
     {ok, {{simple_one_for_one, 0, 1},
