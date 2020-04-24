093e9dfda9a2f8aa8ebe9fdba84a4947bb992b92
Matyas Markovics <matyas@scrapinghub.com>
2020-04-24 12:44:54 +0200

    Adjust worker-supervisor callbacks

--- src/poolboy_worker_supervisor.erl
+++ src/poolboy_worker_supervisor.erl
@@ -4,3 +4,10 @@
                            {error, Reason} when
     Pid    :: pid(),
     Reason :: term().
+
+-callback start_child(integer()) -> {ok, Pid} |
+                                    {error, Reason} when
+    Pid    :: pid(),
+    Reason :: term().
+
+-optional_callbacks([start_child/0, start_child/1]).
